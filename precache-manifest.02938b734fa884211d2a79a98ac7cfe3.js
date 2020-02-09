95d969556d2ba8852666f41068f25b665b689530
Acko <spasic.acko.95@gmail.com>
2020-02-09 20:30:30 +0100

    Updates

---
+++ precache-manifest.02938b734fa884211d2a79a98ac7cfe3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b42c201c1ac2559378db23bff4676cc",
+    "url": "/wwtbam/index.html"
+  },
+  {
+    "revision": "8b73e7cf5d40e2f0014b",
+    "url": "/wwtbam/static/css/2.9437f9af.chunk.css"
+  },
+  {
+    "revision": "39abc4486a7be804eaac",
+    "url": "/wwtbam/static/css/main.855b067a.chunk.css"
+  },
+  {
+    "revision": "8b73e7cf5d40e2f0014b",
+    "url": "/wwtbam/static/js/2.ce60c691.chunk.js"
+  },
+  {
+    "revision": "6b1fbaab5507e10506120199166aab93",
+    "url": "/wwtbam/static/js/2.ce60c691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39abc4486a7be804eaac",
+    "url": "/wwtbam/static/js/main.a610340e.chunk.js"
+  },
+  {
+    "revision": "d57de378004a0cf39362",
+    "url": "/wwtbam/static/js/runtime-main.81031c60.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/wwtbam/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/wwtbam/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/wwtbam/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/wwtbam/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/wwtbam/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/wwtbam/static/media/icons-20.ca7934b3.eot"
+  },
+  {
+    "revision": "9de4364e5ee81388b77db2f34b3632c6",
+    "url": "/wwtbam/static/media/logo.9de4364e.png"
+  },
+  {
+    "revision": "f3ca33dd9bb25f270f5b7bb8d288c755",
+    "url": "/wwtbam/static/media/people.f3ca33dd.svg"
+  },
+  {
+    "revision": "6e3b345b961f6cd74d7b0c4a169f3092",
+    "url": "/wwtbam/static/media/phone.6e3b345b.svg"
+  },
+  {
+    "revision": "849539735c84488fbe981aca57e5e6b9",
+    "url": "/wwtbam/static/media/scale.84953973.svg"
+  }
+]);
